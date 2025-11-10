0b9333086c8ba5a89f890a927121bc9af0055da0
PotatoMaaan <60669730+PotatoMaaan@users.noreply.github.com>
2025-11-10 11:13:12 +0100

    setup starlight docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,29 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import starlight from "@astrojs/starlight";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: { plugins: [tailwindcss()], },
+  vite: { plugins: [tailwindcss()], },
+  integrations: [starlight({
+    title: "Menix - Docs",
+    customCss: [
+      './src/styles/global.css',
+    ],
+    logo: {
+      src: "./src/assets/menix.svg"
+    },
+    social: [
+      {
+        icon: "github",
+        label: "Github",
+        href: "https://github.com/menix-os/menix"
+      }
+    ],
+    editLink: {
+      baseUrl: "https://github.com/menix-os/website/edit/main/"
+    },
+    disable404Route: true
+  })],
 });
---
+++ src/content/docs/docs/getting-started.md
@@ -0,0 +1,12 @@
+---
+title: Getting Started
+description: Learn how to set up a basic installation of Menix
+---
+
+# Hello to the docs
+
+```rs
+test
+```
+
+# amoger foger
