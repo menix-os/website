a459aee877266409b5a4455f009b4704f9870158
PotatoMaaan <60669730+PotatoMaaan@users.noreply.github.com>
2025-11-11 22:32:24 +0100

    add pages action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,33 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: { plugins: [tailwindcss()], },
-  integrations: [starlight({
-    title: "Menix Docs",
-    customCss: [
-      './src/styles/global.css',
-    ],
-    logo: {
-      src: "./src/assets/logos/logo.svg"
-    },
-    social: [
-      {
-        icon: "github",
-        label: "Github",
-        href: "https://github.com/menix-os/menix"
-      }
-    ],
-    editLink: {
-      baseUrl: "https://github.com/menix-os/website/edit/main/"
-    },
-    disable404Route: true,
-    components: {
-      SiteTitle: "./src/components/starlight-overrides/SiteTitle.astro"
-    }
-  })],
+  site: "https://menix-os.org",
+  vite: { plugins: [tailwindcss()] },
+  integrations: [
+    starlight({
+      title: "Menix Docs",
+      customCss: ["./src/styles/global.css"],
+      logo: {
+        src: "./src/assets/logos/logo.svg",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "Github",
+          href: "https://github.com/menix-os/menix",
+        },
+      ],
+      editLink: {
+        baseUrl: "https://github.com/menix-os/website/edit/main/",
+      },
+      disable404Route: true,
+      components: {
+        SiteTitle: "./src/components/starlight-overrides/SiteTitle.astro",
+      },
+    }),
+  ],
 });
